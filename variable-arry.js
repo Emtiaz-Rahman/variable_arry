c827d052114319816eb60b600105d87d832993d2
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-28 18:06:17 +0600

    variable end

---
+++ variable-arry.js
@@ -0,0 +1,10 @@
+// var bottleColor = 'yellow'
+// var waterQuantity = 1;
+// var isFull = false
+
+
+var items = ['Bottle', 'mug', 'paper', 'pen'];
+// var item = items.indexOf('paper');
+items.push('invelop');
+items.push('watch');
+console.log(items);
